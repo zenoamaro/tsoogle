c8a6e070a78088c9b2ee3daf1644f1666794acf2
Yamen <oosaretin@matterway.io>
2024-01-19 15:11:58 +0100

    use typescript checker to compare types

--- example/src/index.ts
+++ example/src/index.ts
@@ -10,4 +10,13 @@ export function getSwearWord(): string {
   return 'heck';
 }
 
-type test =  {a: 1, b: 2} extends {a: 1}? 1 : 0;
+export function getFromStringAndNumber(str: string, num: number): string {
+  return `${num} ${str}`;
+}
+
+export function getFromNumberAndString(num: number, str: string): string {
+  return `${num} ${str}`;
+}
+
+// type test = {a: 1; b: 2} extends {a: 1} ? 1 : 0;
+type test = (a: string, b: number) => string;
--- project/src/index.ts
+++ project/src/index.ts
@@ -1,51 +1,60 @@
-import {readFile} from 'fs/promises';
-import {every, filter, find, head, invoke, map, some} from 'lodash-es'
-import path from 'path';
+import {filter, map} from 'lodash-es';
 import ts from 'typescript';
 import tsm, {SourceFile} from 'ts-morph';
-
 
 console.log('start\n----------------------------------------\n');
 
 const project = new tsm.Project({
-  tsConfigFilePath: "../example/tsconfig.json",
+  tsConfigFilePath: '../example/tsconfig.json'
 });
 
-const typeChecker = project.getTypeChecker();
-
-interface Query {
-  params: string[],
-  returns: string,
-}
+const program = ts.createProgram(
+  project.getSourceFiles().map((sf) => sf.getFilePath()),
+  {}
+);
 
 interface Match {
-  name: string,
+  name: string;
 }
 
-function findByType(source: SourceFile, query: Query): Match[] {
-  const matches = filter(source.getFunctions(), (fn: tsm.FunctionDeclaration) => {
-    const rtype = fn.getReturnType();
-    const argTypes = fn.getParameters();
-    return (
-      every(query.params, (t, i) => t === argTypes[i].getType().getText()) &&
-      query.returns == rtype.getText()
-    )
-  });
+function findByType(source: SourceFile, query: ts.Type): Match[] {
+  const checker = program.getTypeChecker();
+  const matches = filter(
+    source.getFunctions(),
+    (fn: tsm.FunctionDeclaration) => {
+      const fnType = fn.getType().compilerType as ts.Type;
+      const isMatch = checker.isTypeAssignableTo(fnType, query);
+
+      console.log('\n---------------------------------------\n');
+      console.log('isMatch', isMatch);
+      return isMatch;
+    }
+  );
+
   return map(matches, (m) => {
     return {
       name: m.getName() || 'anonymous'
-    }
+    };
   });
 }
+
+const queryString = '(a: string, b: number) => string';
+
+const testSource = project.createSourceFile(
+  'test.ts',
+  `type test = ${queryString}`,
+  {
+    scriptKind: ts.ScriptKind.TS
+  }
+);
+
+const testFnType = testSource.getTypeAliasOrThrow('test').getType();
 
 console.log('\n---------------------------------------\n');
 
 const source = project.getSourceFileOrThrow('index.ts');
 
-const matches = findByType(source, {
-  params: [],
-  returns: 'T'
-});
+const matches = findByType(source, testFnType.compilerType as ts.Type);
 
 console.log(matches);
 
